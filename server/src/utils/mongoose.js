84156a3bd61a962b98203625b40fd7f16253edac
sumant1512 <sumantmishra511@gmail.com>
2025-08-02 03:18:36 +0530

    added .env support

--- server/src/utils/mongoose.js
+++ server/src/utils/mongoose.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const DB_URI = "mongodb://localhost:27017/palsshop";
+
+const username = encodeURIComponent(process.env.MONGO_USERNAME);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+const dbName = process.env.MONGO_DB_NAME;
+
+const MONGO_CONNECTION_STRING = `mongodb+srv://${username}:${password}@cluster0.ivoq6.mongodb.net/${dbName}`;
 
 const connectToMongoDB = async () => {
   try {
     // Connect the client to the server	(optional starti9ng in v4.7)
-    await mongoose.connect(DB_URI);
+    await mongoose.connect(MONGO_CONNECTION_STRING);
 
     // Send a ping to confirm a successful connection
     const db = mongoose.connection.db;
