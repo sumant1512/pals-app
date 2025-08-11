6ed71c871c36ddb9405397287d7441c90dc6bc8f
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-12 00:21:56 +0530

    Delete server/src/routes/+user/orders.js

--- server/src/routes/+user/orders.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-const router = express.Router();
-
-const ordersController = require("./../../controllers/+user/orders");
-
-router.get("/my-orders/:userId", ordersController.getMyOrders);
-router.post("/", ordersController.order);
-
-module.exports = { routes: router };
