f2d3882cf357edb4953e587c3ad2ac70ac8431ed
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-13 00:10:39 +0530

    Create Product.js model

---
+++ server/src/models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  productType: {
+    type: String,
+    required: true,
+    enum: ["Interior", "Exterior"]
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  isShadeEnabled: {
+    type: Boolean,
+    required: true,
+  },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("product", ProductSchema);
+Product.createIndexes();
+module.exports = Product;
+
