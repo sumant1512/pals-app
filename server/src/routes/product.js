1ea72f6bfaff5eea68c53e619d8938705d8ad0b0
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-14 00:39:08 +0530

    Updateed product get api

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -5,7 +5,7 @@ const productController = require("./../controllers/product");
 
 router.post("/add",productController.validateAddProduct , productController.addProduct);
 router.put("/update/:id", productController.updateProduct);
-router.get("/", productController.getProducts);
+router.get("/get", productController.getProducts);
 router.delete("/delete/:id", productController.deleteProduct);
 
 module.exports = { routes: router };
