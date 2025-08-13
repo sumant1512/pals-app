ed62f409ceb9165b652eb5ef71aa61d2a4df6f77
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2025-08-14 00:29:13 +0530

    Update import in product.js controller

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -1,6 +1,6 @@
 const { body, validationResult } = require("express-validator");
-const { ERROR_500 } = require("./../../utils/constant");
-const Product = require("../../models/Products");
+const { ERROR_500 } = require("./../utils/constant");
+const Product = require("../models/Products");
 
 const validateAddProduct = [
   body("productName").notEmpty().withMessage("Product name is required."),
