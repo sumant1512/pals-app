3181535ac806a538a34e6559ecb2a093a0a77cef
sumant1512 <sumantmishra511@gmail.com>
2025-08-29 00:19:50 +0530

    reverted be path

--- client/src/constants/Config.js
+++ client/src/constants/Config.js
@@ -1,2 +1,2 @@
-// export const BE_PATH = "https://pals-app-b3zs.onrender.com";
-export const BE_PATH = "http://localhost:8080";
+export const BE_PATH = "https://pals-app-b3zs.onrender.com";
+// export const BE_PATH = "http://localhost:8080";
