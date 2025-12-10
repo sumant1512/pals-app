fd7f57dee3a6ae619d9f8645328419a9e36c7873
sumant1512 <sumantmishra511@gmail.com>
2025-12-11 00:07:42 +0530

    updated be path

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -2,4 +2,4 @@ export const BE_DEV_PATH = "https://pals-app-b3zs.onrender.com";
 export const BE_PROD_PATH = "https://api.palspaint.com";
 export const BE_LOCAL_PATH = "http://localhost:8080";
 
-const BE_PATH = BE_PROD_PATH;
+export const BE_PATH = BE_PROD_PATH;
